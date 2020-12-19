3c1a7bb7f97767e75f0388d1b01664f16ada2262
uosdevelopers <64471964+uosdevelopers@users.noreply.github.com>
2020-12-19 20:36:04 +0500

    Update adder.py

--- adder.py
+++ adder.py
@@ -115,7 +115,7 @@ for user in users:
             sys.exit("Invalid Mode Selected. Please Try Again.")
         client(InviteToChannelRequest(target_group_entity, [user_to_add]))
         print("Waiting for 60-180 Seconds...")
-        time.sleep(random.randrange(0, 5))
+        time.sleep(80)
     except PeerFloodError:
         print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
         print("Waiting {} seconds".format(SLEEP_TIME_2))
